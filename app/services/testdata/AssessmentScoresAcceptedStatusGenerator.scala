1d25ba9f235f5d159551febccac1bb23b34f3686
Henri Cook <github@henricook.com>
2016-08-17 14:12:45 +0100

    Add the ability to generate fake addresses, and have averaged/evaluated assessment centre results in generated fake data

--- app/services/testdata/AssessmentScoresAcceptedStatusGenerator.scala
+++ app/services/testdata/AssessmentScoresAcceptedStatusGenerator.scala
@@ -18,8 +18,12 @@ package services.testdata
 
 import connectors.testdata.ExchangeObjects.DataGenerationResponse
 import model.ApplicationStatuses
+import model.AssessmentEvaluationCommands.AssessmentPassmarkPreferencesAndScores
 import repositories._
 import repositories.application.GeneralApplicationRepository
+import scheduler.assessment.EvaluateAssessmentScoreJob
+import services.applicationassessment.ApplicationAssessmentService
+import services.passmarksettings.AssessmentCentrePassMarkSettingsService
 import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -28,15 +32,35 @@ import scala.concurrent.Future
 object AssessmentScoresAcceptedStatusGenerator extends AssessmentScoresAcceptedStatusGenerator {
   override val previousStatusGenerator = AssessmentScoresEnteredStatusGenerator
   override val aRepository = applicationRepository
+  override val aaService = ApplicationAssessmentService
+  override val aasRepository = applicationAssessmentScoresRepository
+  override val fpRepository = frameworkPreferenceRepository
+  override val acssService = AssessmentCentrePassMarkSettingsService
 }
 
 trait AssessmentScoresAcceptedStatusGenerator extends ConstructiveGenerator {
   val aRepository: GeneralApplicationRepository
+  val aaService: ApplicationAssessmentService
+  val aasRepository: ApplicationAssessmentScoresRepository
+  val fpRepository: FrameworkPreferenceRepository
+  val acssService: AssessmentCentrePassMarkSettingsService
 
   def generate(generationId: Int, generatorConfig: GeneratorConfig)(implicit hc: HeaderCarrier): Future[DataGenerationResponse] = {
     for {
       candidateInPreviousStatus <- previousStatusGenerator.generate(generationId, generatorConfig)
-      _ <- aRepository.updateStatus(candidateInPreviousStatus.applicationId.get, ApplicationStatuses.AssessmentScoresAccepted)
+      appId = candidateInPreviousStatus.applicationId.get
+      _ <- aRepository.updateStatus(appId, ApplicationStatuses.AssessmentScoresAccepted)
+      scoresAndFeedback <- aasRepository.tryFind(appId)
+      preferences <- fpRepository.tryGetPreferences(appId)
+      latestPassMarks <- acssService.getLatestVersion
+      // Check a pass mark is set for a scheme, otherwise fall over (we need pass marks to accept and score someone)
+      schemeThresholds = latestPassMarks.schemes.head.overallPassMarks.getOrElse(throw new Exception("Assessment centre pass marks need " +
+        "to be set to generate candidates in this status."))
+      _ <- aaService.evaluateAssessmentCandidateScore(
+        AssessmentPassmarkPreferencesAndScores(latestPassMarks, preferences.get, scoresAndFeedback.get),
+        EvaluateAssessmentScoreJob.minimumCompetencyLevelConfig
+      )
+      // candidate scores and feedback and 'preferences'
     } yield {
       candidateInPreviousStatus
     }
