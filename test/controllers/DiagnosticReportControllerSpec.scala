e0b46203bed038784cbe935788791f50ece8b901
Ben Stephenson <bjames.stephenson@gmail.com>
2017-01-25 18:04:18 +0000

    Fix tests

--- test/controllers/DiagnosticReportControllerSpec.scala
+++ test/controllers/DiagnosticReportControllerSpec.scala
@@ -19,9 +19,10 @@ package controllers
 import config.TestFixtureBase
 import model.Exceptions.ApplicationNotFound
 import org.mockito.Mockito._
-import play.api.libs.json.{ JsArray, JsValue, Json }
+import play.api.libs.json.{ JsArray, JsObject, JsValue, Json }
 import play.api.test.Helpers._
 import play.api.test.{ FakeHeaders, FakeRequest, Helpers }
+import repositories.{ ApplicationAssessmentScoresRepository, TestReportRepository }
 import repositories.application.DiagnosticReportingRepository
 import testkit.UnitWithAppSpec
 
@@ -31,25 +32,29 @@ class DiagnosticReportControllerSpec extends UnitWithAppSpec {
 
   "Get application by id" should {
     "return all non-sensitive information about the user application" in new TestFixture {
-      val expectedApplications = List(Json.obj("applicationId" -> "app1", "userId" -> "user1", "frameworkId" -> "FastTrack-2017"))
-      when(mockDiagnosticReportRepository.findByUserId("user1")).thenReturn(Future.successful(expectedApplications))
-      val result = TestableDiagnosticReportingController.getApplicationByUserId("user1")(createOnlineTestRequest(
-        "user1"
+      val expectedApplication = Json.obj("applicationId" -> "app1", "userId" -> "user1", "frameworkId" -> "FastTrack-2017")
+      when(mockDiagnosticReportRepository.findByApplicationId("app1")).thenReturn(Future.successful(expectedApplication))
+      when(mockTestResultRepo.getReportByApplicationId("app1")).thenReturn(Future.successful(None))
+      when(mockAssessmentResultRepo.tryFind("app1")).thenReturn(Future.successful(None))
+
+      val result = TestableDiagnosticReportingController.getApplicationByApplicationId("app1")(createOnlineTestRequest(
+        "app1"
       )).run
 
       val resultJson = contentAsJson(result)
 
       val actualApplications = resultJson.as[JsValue]
       status(result) mustBe OK
-      resultJson mustBe JsArray(expectedApplications)
+      resultJson mustBe expectedApplication
     }
 
     "return NotFound if the user cannot be found" in new TestFixture {
       val IncorrectUserId = "1234"
-      when(mockDiagnosticReportRepository.findByUserId(IncorrectUserId)).thenReturn(Future.failed(
+      when(mockDiagnosticReportRepository.findByApplicationId(IncorrectUserId)).thenReturn(Future.failed(
         ApplicationNotFound(IncorrectUserId)
       ))
-      val result = TestableDiagnosticReportingController.getApplicationByUserId(IncorrectUserId)(createOnlineTestRequest(IncorrectUserId)).run
+      val result = TestableDiagnosticReportingController
+        .getApplicationByApplicationId(IncorrectUserId)(createOnlineTestRequest(IncorrectUserId)).run
 
       status(result) mustBe NOT_FOUND
     }
@@ -57,13 +62,17 @@ class DiagnosticReportControllerSpec extends UnitWithAppSpec {
 
   trait TestFixture extends TestFixtureBase {
     val mockDiagnosticReportRepository = mock[DiagnosticReportingRepository]
+    val mockTestResultRepo = mock[TestReportRepository]
+    val mockAssessmentResultRepo = mock[ApplicationAssessmentScoresRepository]
 
     object TestableDiagnosticReportingController extends DiagnosticReportController {
       val drRepository = mockDiagnosticReportRepository
+      override val trRepository: TestReportRepository = mockTestResultRepo
+      override val assessmentScoresRepo: ApplicationAssessmentScoresRepository = mockAssessmentResultRepo
     }
 
     def createOnlineTestRequest(userId: String) = {
-      FakeRequest(Helpers.GET, controllers.routes.DiagnosticReportController.getApplicationByUserId(userId).url, FakeHeaders(), "")
+      FakeRequest(Helpers.GET, controllers.routes.DiagnosticReportController.getApplicationByApplicationId(userId).url, FakeHeaders(), "")
         .withHeaders("Content-Type" -> "application/json")
     }
   }
