b494e19f8ecfea5d38019a1aab7d2d31014dbd00
Ian Stainer <ian.stainer@valtech.co.uk>
2018-01-23 09:05:36 +0000

    fset-1954 Introduced new 2018 scheme called Policy

--- app/model/Scheme.scala
+++ app/model/Scheme.scala
@@ -22,9 +22,9 @@ import reactivemongo.bson.{ BSON, BSONHandler, BSONString }
 object Scheme extends Enumeration {
   type Scheme = Value
 
-  val Business, Commercial, DigitalAndTechnology, Finance, ProjectDelivery, NewScheme = Value
+  val Business, Commercial, DigitalAndTechnology, Finance, Policy, ProjectDelivery = Value
 
-  val AllSchemes = List(Business, Commercial, DigitalAndTechnology, Finance, ProjectDelivery, NewScheme)
+  val AllSchemes = List(Business, Commercial, DigitalAndTechnology, Finance, Policy, ProjectDelivery)
   require(AllSchemes.toSet == values, "AllSchemes must contain all possible schemes")
 
   implicit val schemeFormat = new Format[Scheme] {
